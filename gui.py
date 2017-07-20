39c1f74be485cd16f9aedaebf3e5c5c30e581fcf
ody1 <alloemail-github@yahoo.gr>
2017-07-20 22:18:49 +0300

    Updated gui.

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import tkinter.font as tkFont
+import tkinter.font as tkfont
 import tkinter.ttk as ttk
 
 outputWindowHeader = ['Name (Folder A)', 'MD5 (Folder A)', 'Name (Folder B)', 'MD5 (Folder B)']
@@ -11,6 +11,56 @@ outputValues = [
     ('nameA5', 'md5A5', 'nameB5', 'md5B5'),
     ('nameA6', 'md5A6', 'nameB6', 'md5B6'),
 ]
+
+
+def sort_by(tree, col, descending):
+    """sort tree contents when a column header is clicked on"""
+    # grab values to sort
+    data = [(tree.set(child, col), child) for child in tree.get_children('')]
+    # if the data to be sorted is numeric change to float
+    # data =  change_numeric(data)
+    # now sort the data in place
+    data.sort(reverse=descending)
+    for ix, item in enumerate(data):
+        tree.move(item[1], '', ix)
+    # switch the heading so it will sort in the opposite direction
+    tree.heading(col, command=lambda col=col: sort_by(tree, col, int(not descending)))
+
+
+class MultiColumnListBox(ttk.Treeview):
+    def __init__(self):
+        ttk.Treeview.__init__(self)
+        self.tree = None
+        self._setup_widgets()
+        self._build_tree()
+
+    def _setup_widgets(self):
+        container = ttk.Frame()
+        container.pack(fill='both', expand=True)
+        # create a treeview with dual scrollbars
+        self.tree = ttk.Treeview(columns=outputWindowHeader, show="headings")
+        vsb = ttk.Scrollbar(orient="vertical", command=self.tree.yview)
+        hsb = ttk.Scrollbar(orient="horizontal", command=self.tree.xview)
+        self.tree.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
+        self.tree.grid(column=0, row=0, sticky='nsew', in_=container)
+        vsb.grid(column=1, row=0, sticky='ns', in_=container)
+        hsb.grid(column=0, row=1, sticky='ew', in_=container)
+        container.grid_columnconfigure(0, weight=1)
+        container.grid_rowconfigure(0, weight=1)
+
+    def _build_tree(self):
+        for col in outputWindowHeader:
+            self.tree.heading(col, text=col.title(), command=lambda c=col: sort_by(self.tree, c, 0))
+            # adjust the column's width to the header string
+            self.tree.column(col, width=tkfont.Font().measure(col.title()))
+
+        for item in outputValues:
+            self.tree.insert('', 'end', values=item)
+            # adjust column's width if necessary to fit each value
+            for ix, val in enumerate(item):
+                col_w = tkfont.Font().measure(val)
+                if self.tree.column(outputWindowHeader[ix], width=None) < col_w:
+                    self.tree.column(outputWindowHeader[ix], width=col_w)
 
 
 class MainWindow(tk.Tk):
@@ -98,9 +148,6 @@ class MainWindow(tk.Tk):
         self.buttonBrowse = tk.Button(self.labelFrameFolderB, text="...", padx=4)
         self.buttonBrowse.pack(side=tk.RIGHT, padx=4)
 
-        self.frameFileInfo = tk.Frame(self)
-        self.frameFileInfo.pack()
-
         ### Checkbuttons Frame
         self.frameCheckbuttons = tk.Frame(self, padx=4, pady=2)
         self.frameCheckbuttons.pack(fill=tk.X, padx=8, pady=2)
@@ -139,53 +186,5 @@ class MainWindow(tk.Tk):
         self.buttonApply = tk.Button(self.frameCheckbuttons, text="Apply", command=self.apply, padx=4)
         self.buttonApply.pack(side=tk.RIGHT)
 
+        ### File info
         self.listbox = MultiColumnListBox()
-
-
-class MultiColumnListBox(ttk.Treeview):
-    def __init__(self):
-        self.tree = None
-        self._setup_widgets()
-        self._build_tree()
-
-    def _setup_widgets(self):
-        container = ttk.Frame()
-        container.pack(fill='both', expand=True)
-        # create a treeview with dual scrollbars
-        self.tree = ttk.Treeview(columns=outputWindowHeader, show="headings")
-        vsb = ttk.Scrollbar(orient="vertical", command=self.tree.yview)
-        hsb = ttk.Scrollbar(orient="horizontal", command=self.tree.xview)
-        self.tree.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
-        self.tree.grid(column=0, row=0, sticky='nsew', in_=container)
-        vsb.grid(column=1, row=0, sticky='ns', in_=container)
-        hsb.grid(column=0, row=1, sticky='ew', in_=container)
-        container.grid_columnconfigure(0, weight=1)
-        container.grid_rowconfigure(0, weight=1)
-
-    def _build_tree(self):
-        for col in outputWindowHeader:
-            self.tree.heading(col, text=col.title(), command=lambda c=col: sortby(self.tree, c, 0))
-            # adjust the column's width to the header string
-            self.tree.column(col, width=tkFont.Font().measure(col.title()))
-
-        for item in outputValues:
-            self.tree.insert('', 'end', values=item)
-            # adjust column's width if necessary to fit each value
-            for ix, val in enumerate(item):
-                col_w = tkFont.Font().measure(val)
-                if self.tree.column(outputWindowHeader[ix], width=None) < col_w:
-                    self.tree.column(outputWindowHeader[ix], width=col_w)
-
-
-def sortby(tree, col, descending):
-    """sort tree contents when a column header is clicked on"""
-    # grab values to sort
-    data = [(tree.set(child, col), child) for child in tree.get_children('')]
-    # if the data to be sorted is numeric change to float
-    # data =  change_numeric(data)
-    # now sort the data in place
-    data.sort(reverse=descending)
-    for ix, item in enumerate(data):
-        tree.move(item[1], '', ix)
-    # switch the heading so it will sort in the opposite direction
-    tree.heading(col, command=lambda col=col: sortby(tree, col, int(not descending)))
